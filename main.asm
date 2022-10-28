041d0b8a3ec085412f59995cd853dd321fa8b644
Animesh Dhakal <dhakalanimesh385@gmail.com>
2022-10-28 15:33:30 +0545

    Function Prologue

--- main.asm
+++ main.asm
@@ -1,20 +1,23 @@
 ; Anything below this is only readable and writable
 section .data
-    msg db "Hello, World!", 10
-    msg_len equ $ - msg
 
 ; Anything below this is only executable
 section .text
     global _start
 
 _start:
-    mov eax, 0x04
-    mov ebx, 1
-    mov ecx, msg
-    mov edx, msg_len
-    int 0x80
-    
+    call func
     mov eax, 1
-    mov ebx, 0
     int 0x80
 
+func:
+    push ebp ; Save the base pointer
+    mov ebp, esp ; Set the base pointer to the stack pointer
+    
+    ; This is called function prologue
+    mov ebx, 10
+
+    mov esp, ebp ; Restore the stack pointer
+    pop ebp ; Restore the base pointer
+    
+    ret
